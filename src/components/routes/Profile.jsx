3568e0f4f8d79122395a5a87f3c7d651d3c4d68c
torobucci <kevintoro254@gmail.com>
2023-04-19 16:46:32 +0300

    display reserved rockets and missions

--- src/components/routes/Profile.jsx
+++ src/components/routes/Profile.jsx
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Container from 'react-bootstrap/esm/Container';
+import ListGroup from 'react-bootstrap/ListGroup';
 
-const Profile = () => (
-  <div />
-);
+const Profile = () => {
+  const { missions } = useSelector((state) => state.missions);
+  const { rockets } = useSelector((state) => state.rockets);
+  const myMissions = missions.filter((mission) => mission.reserved);
+  const myRockets = rockets.filter((mission) => mission.reserved);
+  return (
+    <Container className="d-flex">
+      <Container>
+        <h2>My Missions</h2>
+        <ListGroup>
+          {myMissions.map((mission) => (
+            <ListGroup.Item key={mission.mission_id}>{mission.mission_name}</ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Container>
+      <Container>
+        <h2>My Rockets</h2>
+        <ListGroup>
+          {myRockets.map((rocket) => (
+            <ListGroup.Item key={rocket.id}>{rocket.name}</ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Container>
+    </Container>
+
+  );
+};
 
 export default Profile;
